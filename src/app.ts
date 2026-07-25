24455dee6a722664fd3cc604187b3bd7f8f334d8
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2026-07-25 09:19:05 +0600

    tenant model setup and check api is working or not

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
 import cookieParser from "cookie-parser";
+import httpStatus  from "http-status";
 
 const app: Application = express();
 
@@ -19,4 +20,16 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
+
+
+
+//USER REGISTRATION
+app.post("/api/tenants/register",async(req:Request,res:Response)=>{
+  const payload=req.body;
+  console.log((payload));
+
+  res.status(httpStatus.CREATED).json({
+    message:"User registered successfully"
+  });
+})
 export default app;
