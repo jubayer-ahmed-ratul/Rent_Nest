5163aa9091ac7cf7176e07fa193773c690979fc3
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2026-07-26 09:52:06 +0600

    admin can view all users

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -1,6 +1,20 @@
+import { prisma } from "../../lib/prisma";
 import { updateUserStatusPayload } from "./admin.interface";
 
-const getAllUsersFromDB = async () => {};
+const getAllUsersFromDB = async () => {
+  const tenants = await prisma.tenant.findMany({
+    omit: { password: true },
+    include: { profile: true },
+    orderBy: { createdAt: "desc" },
+  });
+
+  const landlords = await prisma.landlord.findMany({
+    omit: { password: true },
+    orderBy: { createdAt: "desc" },
+  });
+
+  return { tenants, landlords };
+};
 
 const updateUserStatusIntoDB = async (id: string, role: string, payload: updateUserStatusPayload) => {};
 
